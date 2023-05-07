3d3337b0130965e37f42b455010ab47c8aa99d3e
kevin <>
2023-05-07 18:03:30 +0900

    scaffold tuto

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,43 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context){
 
     return MaterialApp(
-      home : Center(
-        child: Container(width : 50, height: 50, color : Colors.blue,)
-      )
-    );
-
+      home :  Scaffold(
+        appBar: AppBar(
+          title: const Text('너와 나의 연결고리'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.access_alarm),
+              tooltip: 'Show alarm Snackbar',
+              onPressed: () {
+                // debugPrint('hi');
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('This is a snackbar')));
+            },
+          )
+        ],),
+        body: const Center(
+          child : Text('여기는 뭘 쓸까?')
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.home),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: const Icon(Icons.more_vert),
+                onPressed: () {},
+              )
+            ],
+          )
+        )
+        ),
+      );
   }
 }
